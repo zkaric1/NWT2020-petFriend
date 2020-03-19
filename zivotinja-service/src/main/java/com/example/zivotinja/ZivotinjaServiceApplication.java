0a1f45635f53953cdf2cfcbfa4f3e038e7f2b9b8
aibrasimov1 <[aibrasimov1@etf.unsa.ba]>
2020-03-19 20:01:24 +0100

    Initial commit

---
+++ zivotinja-service/src/main/java/com/example/zivotinja/ZivotinjaServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.zivotinja;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZivotinjaServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZivotinjaServiceApplication.class, args);
+    }
+
+}
