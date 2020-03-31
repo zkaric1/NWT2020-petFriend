9069eb57710b53b01158633ba0fa3b8d95568619
aibrasimov1 <[aibrasimov1@etf.unsa.ba]>
2020-03-31 22:52:15 +0200

    Dodan service struktura, izbaceno iz controller

--- zivotinja-service/src/main/java/com/example/zivotinja/controller/BolestController.java
+++ zivotinja-service/src/main/java/com/example/zivotinja/controller/BolestController.java
@@ -1,103 +1,62 @@
 package com.example.zivotinja.controller;
-import com.example.zivotinja.assembler.BolestModelAssembler;
 import com.example.zivotinja.model.Bolest;
-import com.example.zivotinja.exception.BolestException;
 import org.springframework.hateoas.EntityModel;
 import org.springframework.web.bind.annotation.*;
-import com.example.zivotinja.repository.BolestRepository;
 import java.util.List;
-import java.util.stream.Collectors;
-import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.*;
 import org.springframework.hateoas.CollectionModel;
-
+import com.example.zivotinja.service.BolestService;
 import javax.validation.Valid;
 
 @RestController
 public class BolestController {
 
-    private BolestRepository bolestRepository;
-    private final BolestModelAssembler assembler;
+    private BolestService bolestService;
 
-    BolestController(BolestRepository repo, BolestModelAssembler assembler) {
-        bolestRepository = repo;
-        this.assembler = assembler;
+    BolestController(BolestService bolestService) {
+        this.bolestService = bolestService;
     }
 
     // Bez Hateoas
     @GetMapping("/bolest")
     List<Bolest> dobaviBolestIme(@RequestParam(value = "ime", required = false) String ime) throws Exception{
-        if (ime != null) {
-            if (bolestRepository.findByName(ime).size() == 0) {
-                throw new Exception("Ne postoji bolest sa trazenim imenom " + ime);
-            }
-            return bolestRepository.findByName(ime);
-        }
-        else {
-            return bolestRepository.findAll();
-        }
+        if (ime != null) return bolestService.findByName(ime);
+        else return bolestService.findAll();
     }
 
     // Sa Hateoas
     @GetMapping("/bolesti")
     public CollectionModel<EntityModel<Bolest>> all() {
-        List<EntityModel<Bolest>> employees = bolestRepository.findAll().stream()
-                .map(assembler::toModel)
-                .collect(Collectors.toList());
-
-        return new CollectionModel<>(employees,
-                linkTo(methodOn(BolestController.class).all()).withSelfRel());
+        return bolestService.findAllHateoas();
     }
 
     @GetMapping("/bolesti/{id}")
-    public EntityModel<Bolest> one(@PathVariable Long id) {
-        Bolest bolest = bolestRepository.findById(id).orElseThrow(() -> new BolestException(id));
-        return assembler.toModel(bolest);
+    public Bolest one(@PathVariable Long id) {
+        return bolestService.findById(id);
+       // return assembler.toModel(bolest);
     }
 
     // DELETE metode
     @DeleteMapping("/bolesti")
     void izbrisiSveBolesti(@RequestParam(value = "ime", required = false) String ime) throws Exception{
-        if (bolestRepository.count() == 0) {
-            throw new Exception("Ne postoji vise bolesti u bazi podataka");
-        }
-        if (ime != null) {
-            bolestRepository.deleteByName(ime);
-        }
-        else {
-            bolestRepository.deleteAll();
-        }
+        if (ime != null) bolestService.deleteByName(ime);
+        else bolestService.deleteAll();
     }
 
     @DeleteMapping ("bolesti/{id}")
     void izbrisiBolest (@PathVariable Long id) throws Exception {
-        if (!bolestRepository.existsById(id)) {
-            throw new Exception("Ne postoji bolest sa id " + id);
-        }
-        bolestRepository.deleteById(id);
+        bolestService.deleteById(id);
     }
 
     // PUT metode
     @PutMapping("/bolesti/{id}")
     Bolest updateBolest(@RequestBody Bolest novaBolest, @PathVariable Long id) throws Exception{
-        if (!bolestRepository.existsById(id)) {
-            throw new Exception ("Ne postoji bolest sa trazenim id " + id);
-        }
-        return bolestRepository.findById(id)
-                .map(bolest -> {
-                    bolest.setIme(novaBolest.getIme());
-                    bolest.setLijek(novaBolest.getLijek());
-                    return bolestRepository.save(bolest);
-                })
-                .orElseGet(() -> {
-                    novaBolest.setId(id);
-                    return bolestRepository.save(novaBolest);
-                });
+        return bolestService.put(novaBolest,id);
     }
 
     // POST metode
     @PostMapping ("/bolesti")
     Bolest novaBolest (@Valid @RequestBody Bolest nBol) {
-        return bolestRepository.save(nBol);
+        return bolestService.post(nBol);
     }
 }
 
--- zivotinja-service/src/main/java/com/example/zivotinja/controller/ZivotinjaController.java
+++ zivotinja-service/src/main/java/com/example/zivotinja/controller/ZivotinjaController.java
@@ -1,107 +1,59 @@
 package com.example.zivotinja.controller;
-import com.example.zivotinja.ZivotinjaServiceApplication;
 import com.example.zivotinja.model.Zivotinja;
-import com.example.zivotinja.exception.ZivotinjaException;
-import com.example.zivotinja.repository.ZivotinjaRepository;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import com.example.zivotinja.service.ZivotinjaService;
 import org.springframework.web.bind.annotation.*;
-
 import javax.validation.Valid;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 @RestController
 public class ZivotinjaController {
-    private ZivotinjaRepository zivotinjaRepository;
-    private static final Logger log = LoggerFactory.getLogger(ZivotinjaServiceApplication.class);
 
-    ZivotinjaController(ZivotinjaRepository repo) {
-        zivotinjaRepository = repo;
+    private ZivotinjaService zivotinjaService;
+
+    ZivotinjaController(ZivotinjaService zivotinjaService) {
+        this.zivotinjaService = zivotinjaService;
     }
 
     // GET metode
 
     @GetMapping("/zivotinje/{id}")
-    public Zivotinja one(@PathVariable Long id) {
-        return zivotinjaRepository.findById(id).orElseThrow(() -> new ZivotinjaException(id));
+    public Zivotinja one(@PathVariable Long id) throws Exception {
+        return zivotinjaService.findById(id);
     }
 
     @GetMapping("zivotinje/{id}/godine")
-    public Map<String, Integer> vratiGodine(@PathVariable Long id) throws Exception{
-        if (!zivotinjaRepository.existsById(id)) {
-            throw new Exception("Ne postoji zivotinja sa id " + id);
-        }
-        List<Zivotinja> ziv = zivotinjaRepository.findByAge(id);
-        HashMap<String, Integer> mapa = new HashMap<>();
-        int godine = 0;
-        for (Zivotinja ob : ziv){
-            godine = ob.getGodine();
-        }
-        mapa.put("godine", godine);
-        return mapa;
+    public Map<String, Integer> vratiGodine(@PathVariable Long id) throws Exception {
+        return zivotinjaService.getGodine(id);
     }
 
     @GetMapping("/zivotinje")
-    public List<Zivotinja> dobaviZivotinjaIme(@RequestParam(value = "ime", required = false) String ime) throws Exception{
-        if (ime != null) {
-            if (zivotinjaRepository.findByName(ime).size() == 0) {
-                throw new Exception("Ne postoji zivotinja sa imenom " + ime);
-            }
-            return zivotinjaRepository.findByName(ime);
-        } else {
-            return zivotinjaRepository.findAll();
-        }
+    public List<Zivotinja> dobaviZivotinjaIme(@RequestParam(value = "ime", required = false) String ime) throws Exception {
+        if (ime != null) return zivotinjaService.findByName(ime);
+        else return zivotinjaService.findAll();
     }
 
     // DELETE metode
     @DeleteMapping("/zivotinje")
     void izbrisiSveZivotinje(@RequestParam(value = "ime", required = false) String ime) throws Exception {
-        if (zivotinjaRepository.count() == 0) {
-            throw new Exception ("Baza ne sadrzi niti jednu zivotinju!");
-        }
-        if (ime != null) zivotinjaRepository.deleteByName(ime);
-        else zivotinjaRepository.deleteAll();
+        if (ime != null) zivotinjaService.deleteByName(ime);
+        else zivotinjaService.deleteAll();
     }
 
     @DeleteMapping("/zivotinje/{id}")
     void izbrisiZivotinju(@PathVariable Long id) throws Exception {
-        if(!zivotinjaRepository.existsById(id)) {
-            throw new Exception ("Ne postoji zivotinja sa id " + id);
-        }
-        zivotinjaRepository.deleteById(id);
+        zivotinjaService.deleteById(id);
     }
 
     // PUT metode
     @PutMapping("/zivotinje/{id}")
     Zivotinja updateZivotinje(@RequestBody Zivotinja novaZivotinja, @PathVariable Long id) throws Exception {
-        if (!zivotinjaRepository.existsById(id)) {
-            throw new Exception ("Zivotinja sa id " + id + " ne postoji u bazi!");
-        }
-        return zivotinjaRepository.findById(id)
-                .map(zivotinja -> {
-                    zivotinja.setSlika(novaZivotinja.getSlika());
-                    zivotinja.setDodatniOpis(novaZivotinja.getDodatniOpis());
-                    zivotinja.setGodine(novaZivotinja.getGodine());
-                    zivotinja.setIme(novaZivotinja.getIme());
-                    zivotinja.setRasa(novaZivotinja.getRasa());
-                    zivotinja.setSpol(novaZivotinja.getSpol());
-                    zivotinja.setTezina(novaZivotinja.getTezina());
-                    zivotinja.setUdomljena(novaZivotinja.isUdomljena());
-                    zivotinja.setVelicina(novaZivotinja.getVelicina());
-                    zivotinja.setVrsta(novaZivotinja.getVrsta());
-                    return zivotinjaRepository.save(zivotinja);
-                })
-                .orElseGet(() -> {
-                    novaZivotinja.setId(id);
-                    return zivotinjaRepository.save(novaZivotinja);
-                });
+        return zivotinjaService.put(novaZivotinja, id);
     }
 
     // POST metode
-    @PostMapping ("/zivotinje")
-    Zivotinja novaZivotinja (@Valid @RequestBody Zivotinja nZivotinja) {
-        return zivotinjaRepository.save(nZivotinja);
+    @PostMapping("/zivotinje")
+    Zivotinja novaZivotinja(@Valid @RequestBody Zivotinja nZivotinja) {
+        return zivotinjaService.post(nZivotinja);
     }
 }
