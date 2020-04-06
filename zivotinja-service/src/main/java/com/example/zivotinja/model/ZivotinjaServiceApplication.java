c9654451bfe04e042c791469d89ae7c764657e35
aibrasimov1 <[aibrasimov1@etf.unsa.ba]>
2020-04-06 16:34:35 +0200

    Dodana eureka

--- zivotinja-service/src/main/java/com/example/zivotinja/model/ZivotinjaServiceApplication.java
+++ zivotinja-service/src/main/java/com/example/zivotinja/model/ZivotinjaServiceApplication.java
@@ -5,11 +5,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
 import com.example.zivotinja.repository.*;
 import com.example.zivotinja.model.*;
 
-
+@EnableDiscoveryClient
 @SpringBootApplication
 public class ZivotinjaServiceApplication {
     private static final Logger log = LoggerFactory.getLogger(ZivotinjaServiceApplication.class);
