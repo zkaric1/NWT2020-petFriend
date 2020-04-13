074844079585fedeade6702cfbabc7898d571e78
zlata1 <zlata.karic@antcolony.io>
2020-04-13 23:20:31 +0200

    user animal

--- korisnik_service/src/main/java/com/etf/korisnik_service/UserServiceApplication.java
+++ korisnik_service/src/main/java/com/etf/korisnik_service/UserServiceApplication.java
@@ -29,52 +29,52 @@ public class UserServiceApplication {
     }
 
     @Bean
-    public CommandLineRunner addDataToDatabase(UserRepository kRepository, AnimalRepository zRepository, RoleRepository uRepository, UserAnimalRepository kzRepository, SurveyRepository surveyRepository, UserSurveyRepository userSurveyRepository) {
+    public CommandLineRunner addDataToDatabase(UserRepository userRepository, AnimalRepository animalRepository, RoleRepository roleRepository, UserAnimalRepository userAnimalRepository, SurveyRepository surveyRepository, UserSurveyRepository userSurveyRepository) {
         return (args) -> {
             //uloge
-            uRepository.save(new Role("administrator"));
-            Role u = uRepository.save(new Role("korisnik"));
-            uRepository.save(new Role("veterinar"));
+            roleRepository.save(new Role("administrator"));
+            Role u = roleRepository.save(new Role("korisnik"));
+            roleRepository.save(new Role("veterinar"));
             log.info("Sve uloge \n");
-            for (Role role : uRepository.findAll()) {
+            for (Role role : roleRepository.findAll()) {
                 log.info(role.getRoleName());
             }
             log.info(" ");
 
             // korisnici
-            User k1 = kRepository.save(new User("ante antic", "1234567899876", u));
+            User k1 = userRepository.save(new User("ante antic", "1234567899876", u));
             k1.setRole(u);
-            User k2 = kRepository.save(new User("amno amnic", "93832979237937", u));
+            User k2 = userRepository.save(new User("amno amnic", "93832979237937", u));
             User newUser = new User("zlata karic", "34324343434", u);
             newUser.setEmail("zkaric1@etf.unsa.ba");
             newUser.setHashPassword("novasifra");
-            kRepository.save(newUser);
-            kRepository.save(new User("marko marulic", "323343432342424", u));
+            userRepository.save(newUser);
+            userRepository.save(new User("marko marulic", "323343432342424", u));
             log.info("Svi korisnici \n");
-            for (User user : kRepository.findAll()) {
+            for (User user : userRepository.findAll()) {
                 log.info(user.getFullName());
             }
 
             log.info(" ");
 
             //zivotinje
-            Animal z1 = zRepository.save(new Animal(1,"mica","macka", "Z"));
-            Animal z2 = zRepository.save(new Animal(2,"laki","pas", "M"));
-            Animal z3 = zRepository.save(new Animal(1,"mica1","macka", "Z"));
-            Animal z4 = zRepository.save(new Animal(1,"mica2","macka", "Z"));
-            Animal z5 = zRepository.save(new Animal(1,"mica3","macka", "Z"));
+            Animal z1 = animalRepository.save(new Animal(1,"mica","macka", "Z"));
+            Animal z2 = animalRepository.save(new Animal(2,"laki","pas", "M"));
+            Animal z3 = animalRepository.save(new Animal(1,"mica1","macka", "Z"));
+            Animal z4 = animalRepository.save(new Animal(1,"mica2","macka", "Z"));
+            Animal z5 = animalRepository.save(new Animal(1,"mica3","macka", "Z"));
             log.info("Sve zivotinje \n");
-            for (Animal animal : zRepository.findAll()) {
+            for (Animal animal : animalRepository.findAll()) {
                 log.info(animal.toString());
             }
             log.info(" ");
 
             //korisnik - zivotinja
-            kzRepository.save(new UserAnimal(k1, z1));
-            kzRepository.save(new UserAnimal(k1,z2));
-            kzRepository.save(new UserAnimal(k2, z2));
+            userAnimalRepository.save(new UserAnimal(k1, z1));
+            userAnimalRepository.save(new UserAnimal(k1,z2));
+            userAnimalRepository.save(new UserAnimal(k2, z2));
             log.info("Sve korisnik-zivotinja \n");
-            for (UserAnimal userAnimal : kzRepository.findAll()) {
+            for (UserAnimal userAnimal : userAnimalRepository.findAll()) {
                 log.info(userAnimal.toString());
             }
 
--- korisnik_service/src/main/java/com/etf/korisnik_service/controller/UserAnimalController.java
+++ korisnik_service/src/main/java/com/etf/korisnik_service/controller/UserAnimalController.java
@@ -5,6 +5,10 @@ import com.etf.korisnik_service.model.UserAnimal;
 import com.etf.korisnik_service.repository.UserAnimalRepository;
 import com.etf.korisnik_service.service.AnimalService;
 import com.etf.korisnik_service.service.UserAnimalService;
+import com.github.underscore.lodash.U;
+import org.json.JSONArray;
+import org.json.JSONObject;
+import org.json.XML;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
@@ -12,12 +16,15 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
 
+import java.util.List;
+
 @RestController
 public class UserAnimalController {
 
     @Autowired
     private RestTemplate restTemplate;
 
+    @Autowired
     private AnimalService animalService;
 
     private UserAnimalService userAnimalService;
@@ -27,13 +34,19 @@ public class UserAnimalController {
     }
 
     @GetMapping(value = "/user_animal/dajZivotinju/{idKorisnika}", produces = "application/xml")
-    public String dajZivotinjuOdKorisnika(@PathVariable Integer idKorisnika) throws Exception {
+    public JSONObject dajZivotinjuOdKorisnika(@PathVariable Integer idKorisnika) throws Exception {
+        String response = restTemplate.exchange("http://zivotinjaService/zivotinje", //a/korisnik"
+                HttpMethod.GET, null,String.class).getBody();
+        JSONObject jsonArray = XML.toJSONObject(response); //U.xmlToJson(response);
+
+        JSONObject json = new JSONObject(jsonArray);
+        return jsonArray;
+    }
+
+    @GetMapping(value = "user_animal/sveZivotinje",produces ="application/xml")
+    public String dajSveZivotinje() throws Exception {
         String response = restTemplate.exchange("http://zivotinjaService/zivotinje", //a/korisnik"
                 HttpMethod.GET, null,String.class).getBody();
         return response;
-//        return new ResponseEntity<>(
-//                response,
-//                HttpStatus.OK
-//        );
     }
 }
