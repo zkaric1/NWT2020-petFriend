a1c856fba74e4e73cf4d3e1a7c2806fad613e90b
zlata1 <zlata.karic@antcolony.io>
2020-04-14 01:54:55 +0200

    role testovi gotovi

--- korisnik_service/src/test/java/com/etf/korisnik_service/RoleControllerTest.java
+++ korisnik_service/src/test/java/com/etf/korisnik_service/RoleControllerTest.java
@@ -94,4 +94,46 @@ public class RoleControllerTest {
                 .andExpect(MockMvcResultMatchers.jsonPath("$.message", Matchers.is("Object not found")));
 
     }
+
+    @org.junit.jupiter.api.Test
+    public void deleteRole() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.delete("/uloga/{id}",2)
+                .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().is2xxSuccessful())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.message", Matchers.is("Uspjesno obrisana uloga sa id-em 2")));
+    }
+
+    @org.junit.jupiter.api.Test
+    public void deleteRoleFailed() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.delete("/uloga/{id}",22)
+                .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().is4xxClientError())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.message", Matchers.is("Object not found")));
+    }
+
+    @org.junit.jupiter.api.Test
+    public void getRoleWithName() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.get("/uloga/naziv")
+                .param("naziv_uloge","korisnik")
+                .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().is2xxSuccessful());
+    }
+
+    @org.junit.jupiter.api.Test
+    public void getRoleWithNameFailed() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.get("/uloga/naziv")
+                .param("naziv_uloge","uloga")
+                .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().is5xxServerError())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.message", Matchers.is("Ne postoji uloga sa unesenim nazivom")));
+    }
+
+    @org.junit.jupiter.api.Test
+    public void deleteAllRoles() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.delete("/uloga/obrisi_sve")
+                .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().is2xxSuccessful())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.message", Matchers.is("Uspjesno obrisane sve uloge")));
+    }
+
 }
