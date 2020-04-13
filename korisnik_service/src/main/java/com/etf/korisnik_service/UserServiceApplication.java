812be02154641e7e788eb4a153e3ed2733a424d3
zlata1 <zlata.karic@antcolony.io>
2020-04-13 09:14:12 +0200

    load balanced

--- korisnik_service/src/main/java/com/etf/korisnik_service/UserServiceApplication.java
+++ korisnik_service/src/main/java/com/etf/korisnik_service/UserServiceApplication.java
@@ -8,6 +8,7 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
@@ -22,6 +23,7 @@ public class UserServiceApplication {
     }
 
     @Bean
+    @LoadBalanced
     public RestTemplate restTemplate() {
         return new RestTemplate();
     }
