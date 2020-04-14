945945cfdfe89cb55d0544b215229c7714b678cc
zlata1 <zlata.karic@antcolony.io>
2020-04-14 22:28:51 +0200

    komunikacija sa zivotinjom

--- korisnik_service/src/main/java/com/etf/korisnik_service/controller/UserAnimalController.java
+++ korisnik_service/src/main/java/com/etf/korisnik_service/controller/UserAnimalController.java
@@ -1,10 +1,13 @@
 package com.etf.korisnik_service.controller;
 
+import com.etf.korisnik_service.DTO.ResponseMessageDTO;
 import com.etf.korisnik_service.model.Animal;
+import com.etf.korisnik_service.model.User;
 import com.etf.korisnik_service.model.UserAnimal;
 import com.etf.korisnik_service.repository.UserAnimalRepository;
 import com.etf.korisnik_service.service.AnimalService;
 import com.etf.korisnik_service.service.UserAnimalService;
+import com.etf.korisnik_service.service.UserService;
 import com.github.underscore.lodash.U;
 import org.json.JSONArray;
 import org.json.JSONObject;
@@ -16,6 +19,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
 
+import java.util.HashMap;
 import java.util.List;
 
 @RestController
@@ -26,6 +30,8 @@ public class UserAnimalController {
 
     @Autowired
     private AnimalService animalService;
+    @Autowired
+    private UserService userService;
 
     private UserAnimalService userAnimalService;
 
@@ -34,19 +40,32 @@ public class UserAnimalController {
     }
 
     @GetMapping(value = "/user_animal/dajZivotinju/{idKorisnika}", produces = "application/xml")
-    public JSONObject dajZivotinjuOdKorisnika(@PathVariable Integer idKorisnika) throws Exception {
-        String response = restTemplate.exchange("http://zivotinjaService/zivotinje", //a/korisnik"
+    public String dajZivotinjuOdKorisnika(@PathVariable Integer idKorisnika) throws Exception {
+        String response = restTemplate.exchange("http://zivotinjaService/zivotinje/korisnik/"+idKorisnika, //a/korisnik"
                 HttpMethod.GET, null,String.class).getBody();
         JSONObject jsonArray = XML.toJSONObject(response); //U.xmlToJson(response);
 
         JSONObject json = new JSONObject(jsonArray);
-        return jsonArray;
+        return response;
     }
 
     @GetMapping(value = "user_animal/sveZivotinje",produces ="application/xml")
     public String dajSveZivotinje() throws Exception {
-        String response = restTemplate.exchange("http://zivotinjaService/zivotinje", //a/korisnik"
+        String response = restTemplate.exchange("http://zivotinjaService/zivotinje",
                 HttpMethod.GET, null,String.class).getBody();
         return response;
+    }
+
+    @PutMapping(value = "/udomljena/{idKorisnika}/{idZivotinje}",produces = "application/json")
+    public HashMap<String,String> udomljenaZivotinja(@PathVariable Integer idKorisnika, @PathVariable Integer idZivotinje) throws Exception {
+        User user =userService.getUserById(idKorisnika);
+        if(user == null) {
+            throw new Exception("Ne postoji korisnik sa id-em "+idKorisnika);
+        }
+        Animal animal = new Animal();
+        animal.setAnimalId(idZivotinje);
+        animalService.sacuvajZivotinju(animal);
+        userAnimalService.spasiPodatke(user,animal);
+        return new ResponseMessageDTO("Uspjesno udomljena zivotinja").getHashMap();
     }
 }
