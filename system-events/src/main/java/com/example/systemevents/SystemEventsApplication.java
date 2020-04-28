079c4ade1d718955a52bb803e6b31ee9f2f2a08a
aibrasimov1 <[aibrasimov1@etf.unsa.ba]>
2020-04-28 15:07:18 +0200

    Dodan system events

---
+++ system-events/src/main/java/com/example/systemevents/SystemEventsApplication.java
@@ -0,0 +1,13 @@
+package com.example.systemevents;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SystemEventsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SystemEventsApplication.class, args);
+    }
+
+}
---
+++ system-events/src/main/java/com/example/systemevents/SystemEventsService.java
@@ -0,0 +1,4 @@
+package com.example.systemevents;
+
+public class SystemEventsService {
+}
