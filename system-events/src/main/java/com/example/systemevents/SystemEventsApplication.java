f31ffafc9015ba6c227738f80042de7be06647d6
aibrasimov1 <[aibrasimov1@etf.unsa.ba]>
2020-05-06 16:33:43 +0200

    Dodan grpc, omougucen server, instaliran kompajler, dodani req/res preko grpc

--- system-events/src/main/java/com/example/systemevents/SystemEventsApplication.java
+++ system-events/src/main/java/com/example/systemevents/SystemEventsApplication.java
@@ -1,13 +1,21 @@
 package com.example.systemevents;
-
+import io.grpc.Server;
+import io.grpc.ServerBuilder;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import java.io.IOException;
 
 @SpringBootApplication
 public class SystemEventsApplication {
 
-    public static void main(String[] args) {
+    private static final Logger log = LoggerFactory.getLogger(SystemEventsApplication.class);
+    public static void main(String[] args) throws IOException, InterruptedException {
         SpringApplication.run(SystemEventsApplication.class, args);
+        Server server = ServerBuilder.forPort(8083).addService(new SystemEventsService()).build();
+        server.start();
+        System.out.println("gRPC server na portu 8083 pokrenut!\n");
+        server.awaitTermination();
     }
-
 }
--- system-events/src/main/java/com/example/systemevents/SystemEventsService.java
+++ system-events/src/main/java/com/example/systemevents/SystemEventsService.java
@@ -1,4 +1,23 @@
 package com.example.systemevents;
+import com.example.systemevents.PetFriend.Request;
+import com.example.systemevents.PetFriend.Response;
+import io.grpc.stub.StreamObserver;
 
-public class SystemEventsService {
+public class SystemEventsService extends SystemEventsGrpc.SystemEventsImplBase {
+    public void start(Request request, StreamObserver<Response> responseObserver) {
+        String Odgovor = new StringBuilder()
+                .append("Servis, ")
+                .append(request.getTimeStampAkcije())
+                .append(request.getNazivMikroservisa())
+                .append(request.getAkcija())
+                .append(request.getNazivResursa())
+                .toString();
+
+        Response response = Response.newBuilder()
+                .setPorukaOdgovora(Odgovor)
+                .build();
+
+        responseObserver.onNext(response);
+        responseObserver.onCompleted();
+    }
 }
