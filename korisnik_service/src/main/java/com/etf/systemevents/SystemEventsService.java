e01025d54c18c2c6f16f89b843d99b89ddf46338
zkaric1 <zkaric1@etf.unsa.ba>
2020-05-12 00:35:00 +0200

    dodan event sistem na korisnik service

---
+++ korisnik_service/src/main/java/com/etf/systemevents/SystemEventsService.java
@@ -0,0 +1,26 @@
+package com.example.systemevents;
+import com.example.systemevents.PetFriend.Request;
+import com.example.systemevents.PetFriend.Response;
+import io.grpc.stub.StreamObserver;
+
+public class SystemEventsService extends SystemEventsGrpc.SystemEventsImplBase {
+
+    @Override
+    public void logAction(Request request, StreamObserver<Response> responseObserver) {
+        StringBuilder Odgovor = new StringBuilder()
+                .append("Vrijeme:" + request.getTimeStampAkcije())
+                .append("Naziv mikroservisa: " + request.getNazivMikroservisa())
+                .append("\n")
+                .append("Tip akcije: " + request.getAkcija())
+                .append("\n")
+                .append("Naziv resursa: " + request.getNazivResursa())
+                .append("\n");
+
+        Response response = Response.newBuilder()
+                .setPorukaOdgovora(Odgovor.toString())
+                .build();
+
+        responseObserver.onNext(response);
+        responseObserver.onCompleted();
+    }
+}
