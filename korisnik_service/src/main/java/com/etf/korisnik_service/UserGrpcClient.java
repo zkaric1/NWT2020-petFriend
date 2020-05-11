c2818dc084dc46c401ae1de59f5c6d39ddfceceb
zkaric1 <zkaric1@etf.unsa.ba>
2020-05-12 01:00:14 +0200

    grpc client

---
+++ korisnik_service/src/main/java/com/etf/korisnik_service/UserGrpcClient.java
@@ -0,0 +1,30 @@
+package com.etf.korisnik_service;
+
+import com.etf.systemevents.PetFriend;
+import com.etf.systemevents.SystemEventsGrpc;
+import com.google.protobuf.Timestamp;
+import io.grpc.ManagedChannel;
+import io.grpc.ManagedChannelBuilder;
+
+import java.time.Instant;
+
+public class UserGrpcClient {
+
+    public static void main (String[] args) {
+        ManagedChannel channel = ManagedChannelBuilder.forAddress("localhost", 8083).usePlaintext().build(); // PORT za gRPV server
+        SystemEventsGrpc.SystemEventsBlockingStub blockStub = SystemEventsGrpc.newBlockingStub(channel);
+        Instant time = Instant.now();
+        Timestamp vrijeme = Timestamp.newBuilder().setSeconds(time.getEpochSecond())
+                .setNanos(time.getNano()).build();
+
+        PetFriend.Request zahtjev = PetFriend.Request.newBuilder()
+                .setTimeStampAkcije(vrijeme)
+                .setNazivMikroservisa("Korisnik MS")
+                .setNazivResursa("Korisnik")
+                .build();
+
+        PetFriend.Response odgovor = blockStub.logAction(zahtjev);
+        System.out.println (odgovor.getPorukaOdgovora());
+        channel.shutdown();
+    }
+}
