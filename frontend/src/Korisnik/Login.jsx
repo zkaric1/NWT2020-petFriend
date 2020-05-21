0fe50c645dcc05691764a3fe48a79c07a7dda15f
zlata1 <zlata.karic@antcolony.io>
2020-05-21 23:37:04 +0200

    radi scss

--- frontend/src/Korisnik/Login.jsx
+++ frontend/src/Korisnik/Login.jsx
@@ -1,6 +1,5 @@
 import React, { Component } from 'react'
-import { Button, FormGroup, FormControl, ControlLabel, FormLabel } from "react-bootstrap";
-import "./login.module.css";
+import "./style.scss";
 
 export class Login extends Component {
     constructor(props) {
@@ -29,37 +28,13 @@ export class Login extends Component {
 
     render() {
         return (
-            // <div className="Login">
-            //     <form onSubmit={this.handleSubmit}>
-            //         <FormGroup controlId="email" bsSize="large">
-            //             <FormLabel>Email</FormLabel>
-            //             <FormControl
-            //                 autoFocus
-            //                 type="email"
-            //                 value={this.state.email}
-            //                 onChange={e => this.setEmail(e.target.value)}
-            //             />
-            //         </FormGroup>
-            //         <FormGroup controlId="password" bsSize="large">
-            //             <FormLabel>Password</FormLabel>
-            //             <FormControl
-            //                 value={this.state.password}
-            //                 onChange={e => this.setPassword(e.target.value)}
-            //                 type="password"
-            //             />
-            //         </FormGroup>
-            //         <Button block bsSize="large" disabled={!this.validateForm()} type="submit">
-            //             Login
-            // </Button>
-            //     </form>
-            // </div>
-            <div>
+            <div className="userDiv"> 
                 <img src="login.png" alt="backgroound"></img>
-            <form onSubmit={this.handleSubmit}>
+            <form  className="loginForma" onSubmit={this.handleSubmit}>
                 <label>Prijavi se</label>
-                <input name="email" type="text" placeholder="Email" />
-                <input name="password" type="password" placeholder="Sifra"/>
-                <button type="submit"> Login</button>
+                <input className="loginInput" name="email" type="text" placeholder="Email" />
+                <input  className="loginInput" name="password" type="password" placeholder="Sifra"/>
+                <button className="loginButton"  type="submit"> Login</button>
             </form>
             </div>
         )
