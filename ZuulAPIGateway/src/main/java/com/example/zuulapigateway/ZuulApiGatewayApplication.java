002a3fa269b42b3ed3a5a6cbe7b63eec5fd2dc31
zlata1 <zlata.karic@antcolony.io>
2020-06-02 01:24:08 +0200

    zuul rip

--- ZuulAPIGateway/src/main/java/com/example/zuulapigateway/ZuulApiGatewayApplication.java
+++ ZuulAPIGateway/src/main/java/com/example/zuulapigateway/ZuulApiGatewayApplication.java
@@ -5,9 +5,10 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 
-@SpringBootApplication
-@EnableDiscoveryClient
+
 @EnableZuulProxy
+@EnableDiscoveryClient
+@SpringBootApplication
 public class ZuulApiGatewayApplication {
 
     public static void main(String[] args) {
